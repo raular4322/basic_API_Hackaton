8ea1001c36f5e79b279cfdb4f9b0e78b0bd2adf7
raular4322 <raular98@hotmail.com>
2019-08-13 22:18:21 +0200

    minor fix

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ const port = process.env.PORT || 3000;
 
 
 mongoose.connect(mongodb, { useNewUrlParser: true, useFindAndModify: false }, (err) => {
-  if (err) {
-    console.log(`ERROR: connecting to Database. ${err}`);
-  } else {
+  if (err) console.log(`ERROR: connecting to Database. ${err}`);
+  else {
     console.log(`Connection to ${mongodb} was succesfull`);
     app.listen(port, () => {
       console.log(`RESTful API server started on: ${port}`);
